14faab4e522cf207512eb8eb6ae0fa6282ceb39c
VangapanduBhagyaLakshmi <bhagyavangapandu@gmail.com>
2023-10-09 10:48:34 +0530

    java script

---
+++ Java Script/new.js
@@ -0,0 +1,33 @@
+// console.dir(document)
+// console.log(document.all)
+// console.log(document.head)
+// console.log(document.body)
+
+// let a=document.getElementById("p1")
+// console.log(a)
+
+// let b=document.getElementsByClassName("abc")
+// console.log(b)
+
+// let c=document.getElementsByTagName("ol")
+// console.log(c)
+
+
+// let d=document.getElementsByName("javascript")
+// console.log(d)
+
+// let e=document.querySelectorall("#p1")
+// console.log(e)
+
+// let f=document.querySelectorAll(".abc #xyz")
+// console.log(f)
+
+// let xyz=document.getElementById("xyz")
+// xyz.style.backgroundColor="aqua"
+// xyz.style.fontSize="30px"
+// xyz.style.color="red"
+// xyz.style.fontStyle="italic"
+
+
+let k=document.querySelector("k")
+k.style.backgroundColor="red"   
